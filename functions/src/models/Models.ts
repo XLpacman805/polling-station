d6b47176701b9cc7d69a4244310ff149157d6377
Johnny Meza <xlpacman805@gmail.com>
2020-09-07 22:33:41 -0700

    removed id from Poll model

--- functions/src/models/Models.ts
+++ functions/src/models/Models.ts
@@ -4,7 +4,6 @@ interface option {
 }
 
 interface poll {
-    id: string;
     authorId: string;
     pollTitle: string;
     options: Array<option>;
@@ -13,14 +12,12 @@ interface poll {
 }
 
 class Poll implements poll {
-    id: string;
     authorId: string;
     pollTitle: string;
     options: Array<option>;
     dateCreated: Date;
     expiry: Date;
     constructor(data: poll) {
-        this.id = data.id;
         this.authorId = data.authorId;
         this.pollTitle = data.pollTitle;
         this.options = data.options;
