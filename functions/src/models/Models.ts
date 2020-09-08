337768ca27637626486e08c156d0dc68d444f41d
Johnny Meza <xlpacman805@gmail.com>
2020-09-07 20:20:41 -0700

    updated poll to include authorId

--- functions/src/models/Models.ts
+++ functions/src/models/Models.ts
@@ -5,6 +5,7 @@ interface option {
 
 interface poll {
     id: string;
+    authorId: string;
     pollTitle: string;
     options: Array<option>;
     dateCreated: Date;
@@ -13,12 +14,14 @@ interface poll {
 
 class Poll implements poll {
     id: string;
+    authorId: string;
     pollTitle: string;
     options: Array<option>;
     dateCreated: Date;
     expiry: Date;
     constructor(data: poll) {
         this.id = data.id;
+        this.authorId = data.authorId;
         this.pollTitle = data.pollTitle;
         this.options = data.options;
         this.dateCreated = data.dateCreated;
