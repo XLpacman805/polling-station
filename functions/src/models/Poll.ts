74e74bb5fed10bd25c5b986733a930c9fb73e51f
Johnny Meza <xlpacman805@gmail.com>
2020-09-07 19:05:44 -0700

    Created Poll model in functions

---
+++ functions/src/models/Poll.ts
@@ -0,0 +1,25 @@
+interface option {
+    text: string,
+    count: number
+}
+
+interface poll {
+    id: string;
+    pollTitle: string;
+    options: Array<option>;
+    expiry: Date;
+}
+
+export default class Poll implements poll {
+    id: string;
+    pollTitle: string;
+    options: Array<option>;
+    expiry: Date;
+    constructor(data: poll) {
+        this.id = data.id;
+        this.pollTitle = data.pollTitle;
+        this.options = data.options;
+        this.expiry = data.expiry;
+    }
+
+}
