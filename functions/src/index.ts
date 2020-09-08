21291d71b9b368f1e5e6c0db0fb3341a26c57f0c
Johnny Meza <xlpacman805@gmail.com>
2020-09-07 19:06:07 -0700

    Created createPoll route with CORS

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+const cors = require('cors')({origin: true});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,10 @@ import * as functions from 'firebase-functions';
 export const helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello from Firebase!");
+});
+
+export const createPoll = functions.https.onRequest((request, response) => {
+   cors(request, response, () => {
+      
+   })
 });
