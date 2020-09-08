c0eb33d549f619f8b6a0f8b403960c181fd89d90
Johnny Meza <xlpacman805@gmail.com>
2020-09-07 20:16:46 -0700

    Updated Poll interface and class

--- functions/src/models/Models.ts
+++ functions/src/models/Models.ts
@@ -7,6 +7,7 @@ interface poll {
     id: string;
     pollTitle: string;
     options: Array<option>;
+    dateCreated: Date;
     expiry: Date;
 }
 
@@ -14,11 +15,13 @@ class Poll implements poll {
     id: string;
     pollTitle: string;
     options: Array<option>;
+    dateCreated: Date;
     expiry: Date;
     constructor(data: poll) {
         this.id = data.id;
         this.pollTitle = data.pollTitle;
         this.options = data.options;
+        this.dateCreated = data.dateCreated;
         this.expiry = data.expiry;
     }
 
